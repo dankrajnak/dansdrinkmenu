1d16ef1dcd72ef4bf8745a0387382a2610c60ed2
Daniel Krajnak <dan.krajnak@gmail.com>
2020-08-22 18:47:29 -0400

    Add favicon, delete unused files, add graphql as peer dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -35,8 +35,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-graphql-codegen`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
+    // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
     `gatsby-plugin-offline`,
   ],
 };
