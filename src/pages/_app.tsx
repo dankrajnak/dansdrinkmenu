6d24b4dcb918b8d11bc6d3a17502297a63ebb7d0
Daniel Krajnak <dan.krajnak@gmail.com>
2020-10-18 18:01:08 -0400

    Revert "Add bugsnag"
    This reverts commit b92b176b95a025edee97de4c83281f37853acfd2.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,10 @@
 import * as React from "react";
 import { AppProps } from "next/app";
-import Bugsnag from "@bugsnag/js";
-import BugsnagPluginReact from "@bugsnag/plugin-react";
 
 import "../styles/global.css";
 
-Bugsnag.start({
-  apiKey: "85abec4484bb1467714b2b2a4e5af606",
-  plugins: [new BugsnagPluginReact()],
-});
-
-const ErrorBoundary =
-  Bugsnag.getPlugin("react")?.createErrorBoundary(React) || React.Fragment;
-
 const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <ErrorBoundary>
-      <Component {...pageProps} />
-    </ErrorBoundary>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
